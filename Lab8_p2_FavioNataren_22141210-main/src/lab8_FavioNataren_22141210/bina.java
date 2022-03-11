1788759cc46a6f497d50266f664a08f4c8dbc166
Favionata16 <98546416+Favionata16@users.noreply.github.com>
2022-03-11 16:20:54 -0600

    commit

---
+++ Lab8_p2_FavioNataren_22141210-main/src/lab8_FavioNataren_22141210/bina.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab8_FavioNataren_22141210;
+
+/**
+ *
+ * @author favio
+ */
+public class bina {
+    
+}
